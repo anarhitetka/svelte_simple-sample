3e7a4886eb13bbba7e5013d2608ed463ac699d4c
anarhitetka <a.urumovska@gmail.com>
2023-01-10 18:18:49 +0100

    Navbar initial code

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,141 @@
+<script>
+    let isMenuOpen = false;
+
+    function toggleMenu() {
+        isMenuOpen = !isMenuOpen;
+    }
+
+    let current = 0;
+    function closeMenu(curr) {
+        isMenuOpen = false;
+        current = curr;
+    }
+
+    
+</script>
+
+<div class="navbar">
+    <a href="/" class="logo">
+        <h1>LOGO</h1>
+    </a>
+    <nav class:open={isMenuOpen}>
+        <ul>
+            <li>
+                <a on:click={() => (closeMenu(0))} class:current={current === 0} href="/">Home</a>
+            </li>
+            <li>
+                <a on:click={() => (closeMenu(1))} class:current={current === 1} href="/">Items</a>
+            </li>
+            <li>
+                <a on:click={() => (closeMenu(2))} class:current={current === 2} href="/">Blog</a>
+            </li>
+            <li>
+                <a on:click={() => (closeMenu(3))} class:current={current === 3} href="/">About</a>
+            </li>
+            <li class="sign-in-btn">
+                <a on:click={() => (closeMenu(-1))} href="/">Sign In</a>
+            </li>
+        </ul>
+    </nav>
+    <button class="burger" on:click={toggleMenu}>
+        <div class="bar--1"/>
+        <div class="bar--2"/>
+        <div class="bar--3"/>
+    </button>
+</div>
+
+<style>
+    .navbar {
+        background-color: var(--dark);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: relative;
+        height: 80px;
+    }
+
+    .logo {
+        margin-left: 6vw; 
+    }
+
+    nav {
+        display: none;
+    }
+
+    nav.open {
+        display: block;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: var(--dark);
+        width: 100%;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        gap: 5vh;
+        margin-top: 2vh;
+        margin-bottom: 4vh;
+        list-style: none;
+        font-size: 1.1rem;
+    }
+
+    .current {
+        position: relative;
+    }
+
+    .current::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: var(--secondary);
+    }
+
+    .sign-in-btn {
+        background-color: var(--primary);
+        padding: 0.4rem 1rem;
+        border-radius: 10px;
+        font-weight: 500;
+    }
+
+    .burger {
+        height: 28px;
+        aspect-ratio: 1;
+        border: 2px solid var(--light);
+        background-color: transparent;
+        border-radius: 5px;
+        margin-right: 6vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .burger > div {
+        height: 2px;
+        width: 14px;
+        background-color: var(--light);
+        position: absolute;
+    }
+    
+    .burger:hover {
+        border: 2px solid var(--secondary);
+    }
+
+    .burger:hover > div {
+        background-color: var(--secondary);
+    }
+
+    .bar--1 {
+        transform: translateY(-5px); 
+    }
+
+    .bar--3 {
+        transform: translateY(5px); 
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar />
+<slot />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
+
+    :global(*) {
+        font-family: 'Poppins', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        color: var(--light);
+    }
+
+    /* color variables  */
+    :global(:root) {
+        --primary: #e03616;
+        --secondary: #f3a712;
+        --dark: #222222;
+        --light: #ebebeb;
+    }
+
+    :global(body) {
+        background-color: #555555;
+    }
+</style>

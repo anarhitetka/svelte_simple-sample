5413380deccb770a7705482ac38cfe7a78dcc31b
anarhitetka <a.urumovska@gmail.com>
2023-01-19 19:36:57 +0100

    WIP: svelte store writable

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,11 +1,26 @@
 <script>
-	export let data;
-	const { teams } = data;
+	// export let data;
+	// const { teams } = data;
+
+	import fetchStore from '../../utils/fetch';
+	const [data, loading, error, get] = fetchStore(`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32`);
+	console.log(data)
+
+
 </script>
 
 <h1>NFL Teams</h1>
-{#each teams as team}
+<!-- {#each teams as team}
 	<h3>{team.abbreviation}</h3>
 	<p>{team.displayName}</p>
 	<a href={`/teams/${team.id}`}>link to team</a>
-{/each}
+{/each} -->
+
+{#if $loading}
+	Loading teams ... 
+{:else if $error}
+	error: {$error}
+{:else}
+	teams loaded
+	<pre>{JSON.stringify($data, null, 2)}</pre>
+{/if}

90a570c316dd52e4514fa3b4cef7dfb1cfd2f8e9
anarhitetka <a.urumovska@gmail.com>
2023-01-20 18:33:15 +0100

    Fix: multiple api requests for teams

--- src/routes/teams/+page.js
+++ src/routes/teams/+page.js
@@ -1,31 +1,45 @@
+import { writable } from "svelte/store";
+
 export const load = async ({ fetch }) => {
-	const fetchTeams = async () => {
-		const teamsRes = await fetch(
-			`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32`
-		);
-		const teamsEndpoints = await teamsRes.json();
+	const loading = writable(false);
+	const error = writable(false);
+	const teamsData = writable({});
 
-		// let teamsEndpointsData = [];
-		let teamsData = [];
-		teamsEndpoints.items.forEach((item) => {
-			fetch(item.$ref)
-				.then((res) => res.json())
-				.then((data) => teamsData.push(data));
-			// return teamsEndpointsData.push(item.$ref);
-		});
+	async function get() {
+		loading.set(true);
+		error.set(false);
+		try {
+			// get endpoints for all teams
+			const teamsRes = await fetch(
+				`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32`
+			);
+			const teamsEndpoints = await teamsRes.json();
 
-		// Promise.all(
-		// 	teamsEndpointsData.map((endpoint) => {
-		// 		fetch(endpoint)
-		// 			.then((res) => res.json())
-		// 			.then((data) => teamsData.push(data));
-		// 	})
-		// );
+			// get data for each team from each endpoint
+			const requests = teamsEndpoints.items.map(async (url) => {
+				const response = await fetch(url.$ref);
+				if (response.ok) return await response.json();
+				else throw new Error('Bad response');
+			});
+			const fetchResults = await Promise.all(requests);
 
-		return teamsData;
-	};
+			teamsData.set(fetchResults);
+
+		} catch (e) {
+			error.set(e);
+		}
+
+		loading.set(false);
+		return [teamsData, loading, error, get];
+	}
+
+	get();
 
 	return {
-		teams: fetchTeams()
-	};
+		teamsData: teamsData,
+		loading: loading,
+		error: error,
+		get: get
+	}
+
 };
--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,26 +1,18 @@
 <script>
-	// export let data;
-	// const { teams } = data;
-
-	import fetchStore from '../../utils/fetch';
-	const [data, loading, error, get] = fetchStore(`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32`);
-	console.log(data)
-
+	export let data;
+	const { teamsData, loading, error, get }  = data;
 
 </script>
 
 <h1>NFL Teams</h1>
-<!-- {#each teams as team}
-	<h3>{team.abbreviation}</h3>
-	<p>{team.displayName}</p>
-	<a href={`/teams/${team.id}`}>link to team</a>
-{/each} -->
-
 {#if $loading}
-	Loading teams ... 
+	loading teams...
 {:else if $error}
 	error: {$error}
 {:else}
-	teams loaded
-	<pre>{JSON.stringify($data, null, 2)}</pre>
+{#each $teamsData as team}
+	<h3>{team.abbreviation}</h3>
+	<p>{team.displayName}</p>
+	<a href={`/teams/${team.id}`}>link to team</a>
+{/each} 
 {/if}

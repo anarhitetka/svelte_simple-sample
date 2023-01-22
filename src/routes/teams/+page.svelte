d13900c1cb4a3308ef29e248c26d6473d4d3cc15
anarhitetka <a.urumovska@gmail.com>
2023-01-22 11:31:20 +0100

    Style team cards

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,18 +1,55 @@
 <script>
 	export let data;
 	const { teamsData, loading, error, get }  = data;
-
 </script>
 
-<h1>NFL Teams</h1>
 {#if $loading}
-	loading teams...
+	<h3>loading teams...</h3>
 {:else if $error}
 	error: {$error}
 {:else}
-{#each $teamsData as team}
-	<h3>{team.abbreviation}</h3>
-	<p>{team.displayName}</p>
-	<a href={`/teams/${team.id}`}>link to team</a>
-{/each} 
+<div class="team-container">
+	{#each $teamsData as team}
+		<div class="team-link">
+			<a href={`/teams/${team.id}`} >
+				<div class="team-card">
+					<div class="team-fullname">
+						<p>{team.displayName}</p>
+					</div>
+					<div class="team-logo-abbreviation">
+						<img src={team.logos[0].href} alt="team logo" width="50"/>
+						<h3>{team.abbreviation}</h3>
+					</div>
+				</div>
+			</a>
+		</div>
+	{/each} 
+</div>
 {/if}
+
+<style>
+	.team-container {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: center;
+	}
+	.team-link {
+		margin: 15px;
+		width: 300px;
+		background-color: rgba(75, 72, 72, 0.8);
+		border-radius: 2px;
+		box-shadow: 5px 5px 5px black;
+	}
+	.team-card {
+		display: flex;
+		padding: 15px;
+		justify-content: space-between;
+	}
+	.team-fullname, .team-logo-abbreviation {
+		padding: 5px;
+	}
+	h3 {
+		text-align: center;	
+	}
+</style>

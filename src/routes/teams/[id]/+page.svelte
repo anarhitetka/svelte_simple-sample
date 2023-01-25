411cf8aa8207eceb8ca549f30306473be8fa8cd6
anarhitetka <a.urumovska@gmail.com>
2023-01-25 16:48:23 +0100

    Style: Team page header

--- src/routes/teams/[id]/+page.svelte
+++ src/routes/teams/[id]/+page.svelte
@@ -14,19 +14,71 @@
 		activeItem = event.detail;
 	}
 
+	const [summaryArr] = team.record.items.filter(item => item.type === 'total');
 </script>
 
-<div>
-	<img src={team.logos[0].href} alt="team logo" height="100" />
-	<span>{team.displayName},</span>
-	<span>{team.standingSummary}</span>
+<div class="team-page">
+
+	<div class="team-header">
+		<div class="team-logo">
+			<img src={team.logos[0].href} alt="team logo" height="100" />
+		</div>
+		<div class="team-info">
+			<p class="team-name">{team.displayName.toUpperCase()}</p>
+			<div class="team-summary">
+				<p>{summaryArr.summary}</p>
+				<li>{team.standingSummary}</li>
+			</div>
+		</div>
+	</div>
+
+	<Tabs tabItems={tabItems} activeItem={activeItem} on:tabChange={triggerTabChange} />
+	{#if activeItem === 'Stats'}
+		<StatsTab items={team.record.items}/>
+	{:else if activeItem === 'Venue'}
+		<VenueTab item={team.franchise.venue}/>
+	{:else}
+		<LinksTab items={team.links} />
+	{/if}
+
 </div>
 
-<Tabs tabItems={tabItems} activeItem={activeItem} on:tabChange={triggerTabChange} />
-{#if activeItem === 'Stats'}
-	<StatsTab items={team.record.items}/>
-{:else if activeItem === 'Venue'}
-	<VenueTab item={team.franchise.venue}/>
-{:else}
-	<LinksTab items={team.links} />
-{/if}
+<style>
+	.team-page {
+		background-color: rgba(0,0,0,0.3);
+	}
+	.team-header {
+		display: flex;
+		padding: 20px;
+	}
+	.team-logo {
+		padding-right: 15px;
+	}
+	.team-info {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+	}
+	.team-name {
+		font-size: 1.5rem;
+	}
+	.team-summary {
+		font-size: 0.8rem;
+		display: flex;
+	}
+	.team-summary > p {
+		padding-right: 15px;
+	}
+	@media (max-width: 340px) {
+		.team-header {
+			flex-direction: column;
+			align-items: center;
+		}
+		.team-name {
+			text-align: center;
+		}
+		.team-summary {
+			display: none;
+		}
+	}
+</style>
